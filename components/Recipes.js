16e5f6b6537d539a4e60b6d59e9170460b12f66c
lenalast <lenalast@gmail.com>
2017-10-13 14:02:46 +0200

    Styling

--- components/Recipes.js
+++ components/Recipes.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, ScrollView, TextInput, Modal, TouchableHighlight} from 'react-native';
+import {
+  StyleSheet, Text, View, Image,
+  ScrollView, TextInput, Modal, TouchableHighlight,
+  FlatList,
+}
+  from 'react-native';
 import axios from 'axios';
 
 class Recipes extends Component {
@@ -7,8 +12,10 @@ class Recipes extends Component {
     super(props);
     this.state = {
       dataSource: [],
+      searchedRecipe: [],
       showThisRecipe: [],
-      modalVisible: false
+      modalVisible: false,
+      text: ""
     }
   }
 
@@ -45,12 +52,20 @@ class Recipes extends Component {
                                          source={require('../assets/food.png')}/> )
   }
 
+  _keyExtractor = (i) => i;
+
   render() {
-    const {dataSource} = this.state
+    const {dataSource, showThisRecipe, modalVisible, text, searchedRecipe} = this.state
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Recipes</Text>
-        <TextInput style={styles.input} placeholder="Search for recipes"/>
+        <TextInput style={styles.input}
+                   placeholder="Search for recipes"
+                   value={text}
+                   // onSubmitEditing={(text) => console.log(text)}
+                   onChangeText={(text) => this.setState({text: text})}
+        />
+        <Text>{text}</Text>
         <ScrollView>
           <View style={styles.recipes}>
             {dataSource}
@@ -58,21 +73,30 @@ class Recipes extends Component {
           </View>
         </ScrollView>
 
-        <Modal visible={this.state.modalVisible}
+        <Modal visible={modalVisible}
                transparent={true}
                animationType="slide"
         >
           <View style={styles.modal}>
-            <View style={styles.modalWrapper}>
-              <TouchableHighlight onPress={() => {
-                this.setState({modalVisible: false})
-              }}>
-                <Image style={styles.modalCancelImg}
-                       source={require('../assets/cancel.png')}/>
-              </TouchableHighlight>
-              <Text>{this.state.showThisRecipe.name}</Text>
-              <Image source={{uri: this.state.showThisRecipe.img}} style={{width: 200, height: 160}}/>
-            </View>
+            <TouchableHighlight onPress={() => {
+              this.setState({modalVisible: false})
+            }}>
+              <Image style={styles.modalCancelImg}
+                     source={require('../assets/left-arrow.png')}/>
+            </TouchableHighlight>
+            <Text style={styles.recipeNameModal}>{showThisRecipe.name}</Text>
+            <Image source={{uri: showThisRecipe.img}} style={styles.modalImg}/>
+            <FlatList
+              data={showThisRecipe.ingredients}
+              keyExtractor={this._keyExtractor}
+              renderItem={
+                ({item}) =>
+                  <View style={styles.ingredientsWrapper}>
+                    <View style={styles.wrapper}>
+                      <Text style={styles.ingredients}>{item}</Text>
+                    </View>
+                  </View>
+              }/>
           </View>
         </Modal>
       </View>
@@ -132,6 +156,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
+  recipeNameModal: {
+    marginLeft: 10,
+    fontSize: 22,
+    fontFamily: 'Futura',
+    color: 'white',
+    textAlign: 'left',
+  },
+  ingredientsWrapper: {
+    width: 200,
+    backgroundColor: 'white',
+  },
+  wrapper: {
+    width: 200,
+  },
+  ingredients: {
+    textAlign: 'left',
+    padding: 10,
+  },
   image: {
     width: 150,
     height: 150,
@@ -140,22 +182,20 @@ const styles = StyleSheet.create({
   modal: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalWrapper: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 300,
-    height: 400,
-    borderRadius: 2,
-    backgroundColor: '#6365A0',
+    paddingTop: 20,
+    width: 380,
+    maxHeight: 610,
+    backgroundColor: '#6365a0',
   },
   modalCancelImg: {
-    width: 22,
-    height: 22,
-    top: -150,
-    right: -130,
-  }
+    width: 18,
+    height: 16,
+    margin: 10,
+    top: 0,
+    left: 0,
+  },
+  modalImg: {
+    width: 380,
+    height: 200,
+  },
 });
